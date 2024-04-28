551d3a61ba33248b7e00e1f425c247d436010c31
m-lair <mlair@heidelberg.edu>
2024-04-28 18:03:23 -0500

    Initial Commit

---
+++ Dungeon Roll/Dungeon_RollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_RollApp.swift
+//  Dungeon Roll
+//
+//  Created by Marcus Lair on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_RollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dungeon RollUITests/Dungeon_RollUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dungeon_RollUITestsLaunchTests.swift
+//  Dungeon RollUITests
+//
+//  Created by Marcus Lair on 4/28/24.
+//
+
+import XCTest
+
+final class Dungeon_RollUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
