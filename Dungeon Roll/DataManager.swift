589c6a6643c82af9b26318a5959d81de0dda3ff7
m-lair <mlair@heidelberg.edu>
2024-04-28 23:58:57 -0500

    add views and write character list code

---
+++ Dungeon Roll/DataManager.swift
@@ -0,0 +1,33 @@
+//
+//  DataManager.swift
+//  Dungeon Roll
+//
+//  Created by Marcus Lair on 4/28/24.
+//
+// structs for data and preview data
+
+import Foundation
+
+struct Character: Identifiable {
+    let id: UUID
+    var name: String
+    var level: Int
+    var attrbs: [String : String]
+    
+    init(id: UUID = UUID(), name: String, level: Int, attrbs: [String : String]) {
+        self.id = id
+        self.name = name
+        self.level = level
+        self.attrbs = attrbs
+    }
+}
+
+extension Character {
+    static var sampleData: [Character] {
+        [
+            Character(name:"dookie", level: 8, attrbs:["DEX" : "12", "Char" : "10"]),
+            Character(name:"pookie", level: 1, attrbs:["DEX" : "2", "Char" : "20"]),
+            Character(name:"wookie", level: 12, attrbs:["DEX" : "24", "Char" : "16"])
+        ]
+    }
+}
---
+++ Dungeon Roll/NewCharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCharacterView.swift
+//  Dungeon Roll
+//
+//  Created by Marcus Lair on 4/28/24.
+//
+
+import SwiftUI
+
+struct NewCharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCharacterView()
+}
