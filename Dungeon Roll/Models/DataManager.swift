5781254f4f6c42916e1d119f3f872603ba841331
m-lair <mlair@heidelberg.edu>
2024-04-29 16:00:32 -0500

    character sample data wont work with this so leaving it out of develop but framework for possible character and attrbs interactions

--- Dungeon Roll/Models/DataManager.swift
+++ Dungeon Roll/Models/DataManager.swift
@@ -12,9 +12,9 @@ class Character: Identifiable {
     let id: UUID
     var name: String
     @Published var level: Int
-    var attrbs: [String : String]
+    @Published var attrbs: [Attribute]
     
-    init(id: UUID = UUID(), name: String, level: Int, attrbs: [String : String]) {
+    init(id: UUID = UUID(), name: String, level: Int, attrbs: [Attribute] ){
         self.id = id
         self.name = name
         self.level = level
@@ -22,16 +22,51 @@ class Character: Identifiable {
     }
 }
 
-class Attributes:Character{
-    
+extension Character {
+    struct Attribute: Identifiable{
+        let id:UUID
+        var name: String
+        var val: Int
+        
+        init(id: UUID=UUID(), name: String, val: Int){
+            self.id = id
+            self.name = name
+            self.val = val
+        /*
+         could do it this way orr
+        var dex:Int
+        var cha:Int
+        var con:Int
+        var str:Int
+        var int:Int
+        var wis:Int
+        
+        
+        init(id: UUID = UUID(), dex: Int, cha: Int, con: Int, str: Int, int: Int, wis: Int) {
+            self.id = id
+            self.dex = dex
+            self.cha = cha
+            self.con = con
+            self.str = str
+            self.int = int
+            self.wis = wis
+            */
+        }
+    }
 }
 
 extension Character {
     static var sampleData: [Character] {
         [
-            Character(name:"dookie", level: 8, attrbs:["DEX" : "12", "Char" : "10"]),
-            Character(name:"pookie", level: 1, attrbs:["DEX" : "2", "Char" : "20"]),
-            Character(name:"wookie", level: 12, attrbs:["DEX" : "24", "Char" : "16"])
+            Character(name:"dookie", 
+                      level: 8,
+                      attrbs: Attributes(dex: 8, cha: 10, con: 12, str: 16, int: 6, wis: 10)),
+            Character(name:"pookie", 
+                      level: 2,
+                      attrbs: Attributes(dex: 8, cha: 10, con: 12, str: 16, int: 6, wis: 12)),
+            Character(name:"wookie", 
+                      level: 12,
+                      attrbs: Attributes(dex: 8, cha: 10, con: 12, str: 16, int: 6, wis: 12))
         ]
     }
 }
