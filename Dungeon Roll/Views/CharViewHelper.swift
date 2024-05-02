c0dadb98a3dd90f81b9dd5258a2e018d026ad11f
m-lair <mlair@heidelberg.edu>
2024-05-01 22:49:19 -0500

    Add some fields for new character page

--- Dungeon Roll/Views/CharViewHelper.swift
+++ Dungeon Roll/Views/CharViewHelper.swift
@@ -13,8 +13,8 @@ import SwiftUI
 struct CharView: View {
     let char: Character
     var body: some View {
+        
         HStack{
-            
             Text(char.name)
                 .font(.headline)
                 .accessibilityAddTraits(.isHeader)
@@ -28,15 +28,62 @@ struct CharView: View {
                     //display stat data??
                     //should it even be a map?
                     //Text((char.attrbs))
+                    //display race/class
         }
     }
 }
 
+struct NewCharacter: View {
+    @State var name = ""
+    @State var level: Int = 1
+    let attrbNames = ["Dexterity", "Charisma", "Strength", "Wisdom", "Intelligence", "Constitution"]
+    var attrbs: [Character.Attribute] = []
+    var body: some View{
+        
+        NavigationView {
+                    Form(content: {
+                         Section(header: Text("Basic Info")) {
+                              HStack{
+                                   TextField("Name", text: $name)
+                                   Picker("Lvl:", selection: $level){
+                                        ForEach(1..<21) {
+                                             Text("\($0)")
+                                        }
+                                   }
+                                   .frame(width: 80.0, height: 10.0)
+                              }
+                              // Secure field
+                         }
+                         Section(header: Text("Attribute Info")) {
+                              VStack{
+                                   ForEach(0..<attrbNames.count, id: \.self) {name in
+                                        Text("\(attrbNames[name])")
+                                        
+                                   }
+                              }
+                        
+                        }
+                        Section {
+                            // Button
+                            Button("Save") {
+                                // DO SOMETHING
+                            }
+                        }
+                    })
+                    .navigationBarTitle("New Character")
+                }
+            }
+        }
+
 struct CharView_Previews: PreviewProvider {
     static var char = Character.sampleData[0]
     static var previews: some View {
-        CharView(char: char)
-            .previewLayout(.fixed(width: 400, height: 60))
+        //CharView(char: char)
+        //    .previewLayout(.fixed(width: 400, height: 60))
+        NewCharacter()
     }
 }
 
+func validForm(form: NewCharacter){
+    
+}
